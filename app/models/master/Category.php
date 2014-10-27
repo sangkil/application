34dc4e045e655b55c832eabfcacbe9d020dffd44
mdmunir <misbahuldmunir@gmail.com>
2014-10-27 11:00:01 +0700

    init

---
+++ app/models/master/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\models\master;
+
+/**
+ * Category
+ *
+ * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ * @since 1.0
+ */
+class Category extends \biz\core\master\models\Category
+{
+    
+}
