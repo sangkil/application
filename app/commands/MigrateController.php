b883e0463ed93f5c8f407cd5b34fcef17e9c8913
mdmunir <misbahuldmunir@gmail.com>
2014-11-02 21:36:25 +0700

    allow migrate (up/down/redo) partial

--- app/commands/MigrateController.php
+++ app/commands/MigrateController.php
@@ -10,14 +10,10 @@ use yii\console\Exception;
  * Description of MigrateController
  *
  * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ * @since 1.0
  */
 class MigrateController extends \yii\console\controllers\MigrateController
 {
-    /**
-     * @var boolean Up, Down and Redo to version without affected to other migration.
-     */
-    public $specialAction = false;
-
     /**
      * @var array
      */
@@ -97,115 +93,129 @@ class MigrateController extends \yii\console\controllers\MigrateController
     }
 
     /**
-     * @inheritdoc
+     * Upgrades the application by applying new migration.
+     * For example,
+     *
+     * ~~~
+     * yii migrate/partial-up 101129_185401                      # using timestamp
+     * yii migrate/partial-up m101129_185401_create_user_table   # using full name
+     * ~~~
+     *
+     * @param string $version the version at which the migration history should be marked.
+     * This can be either the timestamp or the full name of the migration.
+     * @return int CLI exit code
+     * @throws Exception if the version argument is invalid or the version cannot be found.
      */
-    public function options($actionID)
+    public function actionPartialUp($version)
     {
-        $options = parent::options($actionID);
-        if (in_array($actionID, ['up', 'down', 'redo'])) {
-            $options = array_merge($options, ['specialAction']);
+        $originalVersion = $version;
+        if (preg_match('/^m?(\d{6}_\d{6})(_.*?)?$/', $version, $matches)) {
+            $version = 'm' . $matches[1];
+        } else {
+            throw new Exception("The version argument must be either a timestamp (e.g. 101129_185401)\nor the full name of a migration (e.g. m101129_185401_create_user_table).");
         }
-        return $options;
+
+        $migrations = $this->getNewMigrations();
+        foreach ($migrations as $migration) {
+            if (strpos($migration, $version . '_') === 0) {
+                if ($this->confirm("Apply the $migration migration?")) {
+                    if (!$this->migrateUp($migration)) {
+                        echo "\nMigration failed.\n";
+
+                        return self::EXIT_CODE_ERROR;
+                    }
+                    return self::EXIT_CODE_NORMAL;
+                }
+                return;
+            }
+        }
+        throw new Exception("Unable to find the version '$originalVersion'.");
     }
 
     /**
-     * @inheritdoc
+     * Downgrades the application by reverting old migration.
+     * For example,
+     *
+     * ~~~
+     * yii migrate/partial-down 101129_185401                      # using timestamp
+     * yii migrate/partial-down m101129_185401_create_user_table   # using full name
+     * ~~~
+     *
+     * @param string $version the version at which the migration history should be marked.
+     * This can be either the timestamp or the full name of the migration.
+     * @return int CLI exit code
+     * @throws Exception if the version argument is invalid or the version cannot be found.
      */
-    public function actionUp($limit = 0)
+    public function actionPartialDown($version)
     {
-        if ($this->specialAction) {
-            $version = $limit;
-            if (preg_match('/^m?(\d{6}_\d{6})(_.*?)?$/', $version, $matches)) {
-                $version = 'm' . $matches[1];
-            } else {
-                throw new Exception("The version argument must be either a timestamp (e.g. 101129_185401)\nor the full name of a migration (e.g. m101129_185401_create_user_table).");
-            }
-
-            $migrations = $this->getNewMigrations();
-            foreach ($migrations as $migration) {
-                if (strpos($migration, $version . '_') === 0) {
-                    if ($this->confirm("Apply the $migration migration?")) {
-                        if (!$this->migrateUp($migration)) {
-                            echo "\nMigration failed.\n";
-
-                            return self::EXIT_CODE_ERROR;
-                        }
-                        return self::EXIT_CODE_NORMAL;
-                    }
-                    return;
-                }
-            }
-            throw new Exception("Unable to find the version '$limit'.");
+        $originalVersion = $version;
+        if (preg_match('/^m?(\d{6}_\d{6})(_.*?)?$/', $version, $matches)) {
+            $version = 'm' . $matches[1];
+        } else {
+            throw new Exception("The version argument must be either a timestamp (e.g. 101129_185401)\nor the full name of a migration (e.g. m101129_185401_create_user_table).");
         }
-        return parent::actionUp($limit);
+
+        $migrations = array_keys($this->getMigrationHistory(null));
+        foreach ($migrations as $migration) {
+            if (strpos($migration, $version . '_') === 0) {
+                if ($this->confirm("Revert the $migration migration?")) {
+                    if (!$this->migrateDown($migration)) {
+                        echo "\nMigration failed.\n";
+
+                        return self::EXIT_CODE_ERROR;
+                    }
+                    return self::EXIT_CODE_NORMAL;
+                }
+                return;
+            }
+        }
+        throw new Exception("Unable to find the version '$originalVersion'.");
     }
 
     /**
-     * @inheritdoc
+     * Redoes partial migration.
+     *
+     * This command will first revert the specified migrations, and then apply
+     * them again. For example,
+     *
+     * ~~~
+     * yii migrate/partial-redo 101129_185401                      # using timestamp
+     * yii migrate/partial-redo m101129_185401_create_user_table   # using full name
+     * ~~~
+     *
+     * @param string $version the version at which the migration history should be marked.
+     * This can be either the timestamp or the full name of the migration.
+     * @return int CLI exit code
+     * @throws Exception if the version argument is invalid or the version cannot be found.
      */
-    public function actionDown($limit = 1)
+    public function actionPartialRedo($version)
     {
-        if ($this->specialAction) {
-            $version = $limit;
-            if (preg_match('/^m?(\d{6}_\d{6})(_.*?)?$/', $version, $matches)) {
-                $version = 'm' . $matches[1];
-            } else {
-                throw new Exception("The version argument must be either a timestamp (e.g. 101129_185401)\nor the full name of a migration (e.g. m101129_185401_create_user_table).");
-            }
-
-            $migrations = array_keys($this->getMigrationHistory(null));
-            foreach ($migrations as $migration) {
-                if (strpos($migration, $version . '_') === 0) {
-                    if ($this->confirm("Revert the $migration migration?")) {
-                        if (!$this->migrateDown($migration)) {
-                            echo "\nMigration failed.\n";
-
-                            return self::EXIT_CODE_ERROR;
-                        }
-                        return self::EXIT_CODE_NORMAL;
-                    }
-                    return;
-                }
-            }
-            throw new Exception("Unable to find the version '$limit'.");
+        $originalVersion = $version;
+        if (preg_match('/^m?(\d{6}_\d{6})(_.*?)?$/', $version, $matches)) {
+            $version = 'm' . $matches[1];
+        } else {
+            throw new Exception("The version argument must be either a timestamp (e.g. 101129_185401)\nor the full name of a migration (e.g. m101129_185401_create_user_table).");
         }
-        return parent::actionDown($limit);
-    }
 
-    /**
-     * @inheritdoc
-     */
-    public function actionRedo($limit = 1)
-    {
-        if ($this->specialAction) {
-            $version = $limit;
-            if (preg_match('/^m?(\d{6}_\d{6})(_.*?)?$/', $version, $matches)) {
-                $version = 'm' . $matches[1];
-            } else {
-                throw new Exception("The version argument must be either a timestamp (e.g. 101129_185401)\nor the full name of a migration (e.g. m101129_185401_create_user_table).");
-            }
+        $migrations = array_keys($this->getMigrationHistory(null));
+        foreach ($migrations as $migration) {
+            if (strpos($migration, $version . '_') === 0) {
+                if ($this->confirm("Redo the $migration migration?")) {
+                    if (!$this->migrateDown($migration)) {
+                        echo "\nMigration failed.\n";
 
-            $migrations = array_keys($this->getMigrationHistory(null));
-            foreach ($migrations as $migration) {
-                if (strpos($migration, $version . '_') === 0) {
-                    if ($this->confirm("Redo the $migration migration?")) {
-                        if (!$this->migrateDown($migration)) {
-                            echo "\nMigration failed.\n";
-
-                            return self::EXIT_CODE_ERROR;
-                        }
-                        if (!$this->migrateUp($migration)) {
-                            echo "\nMigration failed.\n";
-
-                            return self::EXIT_CODE_ERROR;
-                        }
-                        return self::EXIT_CODE_NORMAL;
+                        return self::EXIT_CODE_ERROR;
                     }
-                    return;
+                    if (!$this->migrateUp($migration)) {
+                        echo "\nMigration failed.\n";
+
+                        return self::EXIT_CODE_ERROR;
+                    }
+                    return self::EXIT_CODE_NORMAL;
                 }
+                return;
             }
-            throw new Exception("Unable to find the version '$limit'.");
         }
-        return parent::actionDown($limit);
+        throw new Exception("Unable to find the version '$originalVersion'.");
     }
 }
