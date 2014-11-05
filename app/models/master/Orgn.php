ee90c31456ce2694b6d37dffcaa6815e0ff90758
samsung-pc <samsung-pc@SangkilSoft>
2014-11-05 11:16:33 +0700

    modify master

--- app/models/master/Orgn.php
+++ app/models/master/Orgn.php
@@ -1,12 +1,19 @@
 <?php
+
 namespace app\models\master;
+
 /**
  * Orgn
  *
  * @author Misbahul D Munir <misbahuldmunir@gmail.com>
  * @since 1.0
  */
-class Orgn extends \biz\core\master\models\Orgn
-{
-    
+class Orgn extends \biz\core\master\models\Orgn {
+
+    public function rules() {
+        $dRule = parent::rules();
+        $dRule[] = [['code'], 'unique'];
+        return $dRule;
+    }
+
 }
