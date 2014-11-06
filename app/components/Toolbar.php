6acbac0d290230588cce8340e041916f6d6c6c4c
samsung-pc <samsung-pc@SangkilSoft>
2014-11-06 15:22:17 +0700

    modify admin views

--- app/components/Toolbar.php
+++ app/components/Toolbar.php
@@ -8,9 +8,7 @@ namespace app\components;
  */
 
 /**
- * Description of toolbar
- *
- * @author samsung-pc
+ * Example
  * echo Toolbar::widget([
   'items' => [
   // Important: you need to specify url as 'controller/action',
@@ -36,11 +34,13 @@ class Toolbar extends \yii\base\Widget {
     public function init() {
         parent::init();
         $result = Html::beginTag('div', ['class'=>'btn-group']);
-        foreach ($this->items as $item) {
-            $result .= $this->renderItem($item);
-        }
+        $result .= Html::a('<i class="fa fa-plus-square"></i> Create', ['create'], ['class' => 'btn btn-success btn-sm']);
+        $result .= Html::a('<i class="fa fa-search"></i> Detail', ['view', 'id' => $model->id], ['class' => 'btn bg-navy btn-sm']);
+        $result .= Html::a('<i class="fa fa-pencil"></i> Update', ['update', 'id' => $model->id], ['class' => 'btn btn-warning btn-sm']);
+        $result .= Html::a('<i class="fa fa-trash-o"></i> Delete', ['delete', 'id' => $model->id], ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]);
+        $result .= Html::a('<i class="fa fa-list"></i> List', ['index'], ['class' => 'btn btn-info btn-sm']);
         $result .= Html::endTag('div');
-        echo $result;
+        return $result;
     }
 
     protected function renderItem($item) {
