83ddf06d59dc3dc37a8af9fafc57c4ba9d4ec608
samsung-pc <samsung-pc@SangkilSoft>
2014-11-06 12:00:10 +0700

    modify admin views

--- app/views/admin/menu/_form.php
+++ app/views/admin/menu/_form.php
@@ -9,42 +9,34 @@ use mdm\admin\models\Menu;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="col-lg-8 menu-form">
-    <?php $form = ActiveForm::begin(); ?>
-    <div class="box box-info">
-        <div class="box-body">
+<?php $form = ActiveForm::begin(); ?>
+<div class="box box-info menu-form">
+    <div class="box-body">
+        <?= $form->field($model, 'name')->textInput(['maxlength' => 128]) ?>
+        <?=
+        $form->field($model, 'parent_name')->widget('yii\jui\AutoComplete', [
+            'options' => ['class' => 'form-control'],
+            'clientOptions' => [
+                'source' => Menu::find()->select(['name'])->column()
+            ]
+        ])
+        ?>
 
-            <?= $form->field($model, 'name')->textInput(['maxlength' => 128]) ?>
-
-            <?=
-            $form->field($model, 'parent_name')->widget('yii\jui\AutoComplete', [
-                'options' => ['class' => 'form-control'],
-                'clientOptions' => [
-                    'source' => Menu::find()->select(['name'])->column()
-                ]
-            ])
-            ?>
-
-            <?=
-            $form->field($model, 'route')->widget('yii\jui\AutoComplete', [
-                'options' => ['class' => 'form-control'],
-                'clientOptions' => [
-                    'source' => Menu::getSavedRoutes()
-                ]
-            ])
-            ?>
-
-            <?= $form->field($model, 'order')->input('number') ?>
-
-            <?= $form->field($model, 'data')->textarea(['rows' => 4]) ?>
-
-        </div>
-        <div class="box-footer">
-            <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-        </div>
+        <?=
+        $form->field($model, 'route')->widget('yii\jui\AutoComplete', [
+            'options' => ['class' => 'form-control'],
+            'clientOptions' => [
+                'source' => Menu::getSavedRoutes()
+            ]
+        ])
+        ?>
+        <?= $form->field($model, 'order')->input('number') ?>
+        <?= $form->field($model, 'data')->textarea(['rows' => 4]) ?>
     </div>
-
-
-    <?php ActiveForm::end(); ?>
-
+    <div class="box-footer">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
 </div>
+
+<?php ActiveForm::end(); ?>
+
