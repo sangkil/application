5c4d04bf862d41a0731304c67cd0531c19cf39cf
samsung-pc <samsung-pc@SangkilSoft>
2014-11-06 15:22:41 +0700

    modify admin views

--- app/views/master/orgn/update.php
+++ app/views/master/orgn/update.php
@@ -12,21 +12,9 @@ $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="col-lg-8 orgn-update">
-    <div class="btn-group">
-        <?php
-        $action = $this->context->action->id;
-        $visible_create = in_array($action, array('update', 'view', 'index'));
-        $visible_delete = in_array($action, array('update', 'view'));
-        $visible_list = in_array($action, array('create', 'update', 'view'));
-        $visible_view = in_array($action, array('update'));
-        $visible_update = in_array($action, array('view'));
-        ?>
-        <?= ($visible_create) ? Html::a('<i class="fa fa-plus-square"></i> Create', ['create'], ['class' => 'btn btn-success btn-sm']) : ''; ?> 
-        <?= ($visible_view) ? Html::a('<i class="fa fa-search"></i> Detail', ['view', 'id' => $model->id], ['class' => 'btn bg-navy btn-sm']) : '' ?> 
-        <?= ($visible_update) ? Html::a('<i class="fa fa-pencil"></i> Update', ['update', 'id' => $model->id], ['class' => 'btn btn-warning btn-sm']) : '' ?> 
-        <?= ($visible_delete) ? Html::a('<i class="fa fa-trash-o"></i> Delete', ['delete', 'id' => $model->id], ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]) : '' ?> 
-        <?= ($visible_list) ? Html::a('<i class="fa fa-list"></i> List', ['index'], ['class' => 'btn btn-info btn-sm']) : '' ?>
-    </div> 
+    <?php
+    echo Toolbar::widget();
+    ?>
     <br><br>
     <?=
     $this->render('_form', [
