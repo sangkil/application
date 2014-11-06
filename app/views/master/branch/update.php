6169ec86271cd0fbad460c1caa24036775c93a2a
samsung-pc <samsung-pc@SangkilSoft>
2014-11-06 16:07:49 +0700

    modify admin views

--- app/views/master/branch/update.php
+++ app/views/master/branch/update.php
@@ -11,22 +11,15 @@ $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="col-lg-8 branch-update">
-<div class="btn-group pull-right">
-        <?php
-        $action = $this->context->action->id;
-        $visible_create = in_array($action, array('update', 'view', 'index'));
-        $visible_delete = in_array($action, array('update', 'view'));
-        $visible_list = in_array($action, array('create', 'update', 'view'));
-        $visible_view = in_array($action, array('update'));
-        $visible_update = in_array($action, array('view'));
-        ?>
-        <?= ($visible_create) ? Html::a('<i class="fa fa-plus-square"></i> Create', ['create'], ['class' => 'btn btn-success btn-sm']) : ''; ?> 
-        <?= ($visible_view) ? Html::a('<i class="fa fa-search"></i> Detail', ['view', 'id' => $model->id], ['class' => 'btn bg-navy btn-sm']) : '' ?> 
-        <?= ($visible_update) ? Html::a('<i class="fa fa-pencil"></i> Update', ['update', 'id' => $model->id], ['class' => 'btn btn-warning btn-sm']) : '' ?> 
-        <?= ($visible_delete) ? Html::a('Delete', ['delete', 'id' => $model->id], ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]) : '' ?> 
-        <?= ($visible_list) ? Html::a('<i class="fa fa-list"></i> List', ['index'], ['class' => 'btn btn-info btn-sm']) : '' ?>
-    </div>  
-    <br><br>
+<?php
+    echo Toolbar::widget(['items' => [
+            ['label' => 'Create', 'url' => ['create'], 'icon' => 'fa fa-plus-square', 'linkOptions' => ['class' => 'btn btn-success btn-sm']],
+            ['label' => 'Detail', 'url' => ['view', 'id' => $model->id],'icon' => 'fa fa-search', 'linkOptions' => ['class' => 'btn bg-navy btn-sm']],
+            //['label' => 'Update', 'url' => ['update', 'id' => $model->id],'icon' => 'fa fa-pencil', 'linkOptions' => ['class' => 'btn btn-warning btn-sm']],
+            ['label' => 'Delete', 'url' => ['delete', 'id' => $model->id],'icon' => 'fa fa-trash-o', 'linkOptions' => ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]],
+            ['label' => 'List', 'url' => ['index'],'icon' => 'fa fa-list', 'linkOptions' => ['class' => 'btn btn-info btn-sm']]
+    ]]);
+    ?>
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
--- app/views/master/orgn/view.php
+++ app/views/master/orgn/view.php
@@ -11,22 +11,15 @@ $this->params['breadcrumbs'][] = ['label' => 'Orgns', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="col-lg-8">
-    <div class="btn-group">
-        <?php  
-            $action = $this->context->action->id;
-            $visible_create = in_array($action, array('update','view','index'));
-            $visible_delete = in_array($action, array('update','view'));
-            $visible_list = in_array($action, array('create','update','view'));
-            $visible_view = in_array($action, array('update'));
-            $visible_update = in_array($action, array('view'));
-        ?>
-        <?= ($visible_create)? Html::a('<i class="fa fa-plus-square"></i> Create', ['create'], ['class' => 'btn btn-success btn-sm']):''; ?> 
-        <?= ($visible_view)? Html::a('<i class="fa fa-search"></i> Detail', ['view','id'=>$model->id], ['class' => 'btn bg-navy btn-sm']):'' ?> 
-        <?= ($visible_update)? Html::a('<i class="fa fa-pencil"></i> Update', ['update','id'=>$model->id], ['class' => 'btn btn-warning btn-sm']):'' ?> 
-        <?= ($visible_delete) ? Html::a('<i class="fa fa-trash-o"></i> Delete', ['delete', 'id' => $model->id], ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]) : '' ?> 
-        <?= ($visible_list)? Html::a('<i class="fa fa-list"></i> List', ['index'], ['class' => 'btn btn-info btn-sm']):'' ?>
-    </div>  
-    <br><br>
+    <?php
+    echo Toolbar::widget(['items' => [
+            ['label' => 'Create', 'url' => ['create'], 'icon' => 'fa fa-plus-square', 'linkOptions' => ['class' => 'btn btn-success btn-sm']],
+            //['label' => 'Detail', 'url' => ['view', 'id' => $model->id],'icon' => 'fa fa-search', 'linkOptions' => ['class' => 'btn bg-navy btn-sm']],
+            ['label' => 'Update', 'url' => ['update', 'id' => $model->id],'icon' => 'fa fa-pencil', 'linkOptions' => ['class' => 'btn btn-warning btn-sm']],
+            ['label' => 'Delete', 'url' => ['delete', 'id' => $model->id],'icon' => 'fa fa-trash-o', 'linkOptions' => ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]],
+            ['label' => 'List', 'url' => ['index'],'icon' => 'fa fa-list', 'linkOptions' => ['class' => 'btn btn-info btn-sm']]
+    ]]);
+    ?>
     <div class="box box-info orgn-view">
         <div class="box-body no-padding">
             <?=
