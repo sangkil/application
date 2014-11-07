9675e6170b2f2487149d83b1546eccfcdc744419
samsung-pc <samsung-pc@SangkilSoft>
2014-11-07 13:44:47 +0700

    setup for yii2-user

---
+++ app/views/user/admin/create.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * This file is part of the Dektrium project.
+ *
+ * (c) Dektrium project <http://github.com/dektrium>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/**
+ * @var yii\web\View $this
+ * @var dektrium\user\models\User $model
+ */
+$this->title = Yii::t('user', 'Create a user account');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('user', 'Users'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="alert alert-info">
+    <?= Yii::t('user', 'Password and username will be sent to user by email') ?>.
+    <?= Yii::t('user', 'If you want password to be generated automatically leave its field empty') ?>.
+</div>
+<?php echo $this->render('flash') ?>
+<?php $form = ActiveForm::begin(); ?>
+<div class="box box-primary">
+    <div class="box-header">
+        <?= Html::encode($this->title) ?>
+    </div>
+    <div class="box-body">
+
+        <?= $form->field($model, 'username')->textInput(['maxlength' => 25, 'autofocus' => true]) ?>
+
+        <?= $form->field($model, 'email')->textInput(['maxlength' => 255]) ?>
+
+        <?= $form->field($model, 'password')->passwordInput() ?>
+
+    </div>
+    <div class="box-footer">
+        <?= Html::submitButton(Yii::t('user', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+</div>
