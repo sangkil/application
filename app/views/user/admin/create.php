a6c5291d4f5062798b3daeb4bac54e229ee316f5
samsung-pc <samsung-pc@SangkilSoft>
2014-11-07 21:27:12 +0700

    setup for yii2-user

--- app/views/user/admin/create.php
+++ app/views/user/admin/create.php
@@ -19,13 +19,13 @@ $this->title = Yii::t('user', 'Create a user account');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('user', 'Users'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="alert alert-info">
-    <?= Yii::t('user', 'Password and username will be sent to user by email') ?>.
-    <?= Yii::t('user', 'If you want password to be generated automatically leave its field empty') ?>.
-</div>
-<?php echo $this->render('flash') ?>
-<?php $form = ActiveForm::begin(); ?>
 <div class="col-lg-8">
+    <div class="alert alert-info">
+        <?= Yii::t('user', 'Password and username will be sent to user by email') ?>.
+        <?= Yii::t('user', 'If you want password to be generated automatically leave its field empty') ?>.
+    </div>
+    <?php echo $this->render('flash') ?>
+    <?php $form = ActiveForm::begin(); ?>
     <div class="box box-primary">
         <div class="box-header">
             <?= Html::encode($this->title) ?>
@@ -43,6 +43,6 @@ $this->params['breadcrumbs'][] = $this->title;
             <?= Html::submitButton(Yii::t('user', 'Save'), ['class' => 'btn btn-success']) ?>
         </div>
 
-        <?php ActiveForm::end(); ?>
     </div>
+    <?php ActiveForm::end(); ?>
 </div>
