5c5c88f726a949e056ac60f6f491de90961780c5
mdmunir <misbahuldmunir@gmail.com>
2014-11-18 10:07:21 +0700

    sample data

--- app/commands/SampleDataController.php
+++ app/commands/SampleDataController.php
@@ -5,10 +5,14 @@ namespace app\commands;
 use Yii;
 use yii\db\Connection;
 use yii\console\Exception;
+use yii\db\Expression;
+use yii\helpers\Console;
 
 /**
  * SampleDataController
  *
+ * @property \Faker\Generator $generator
+ * 
  * @author Misbahul D Munir <misbahuldmunir@gmail.com>
  * @since 1.0
  */
@@ -51,31 +55,215 @@ class SampleDataController extends \yii\console\Controller
      */
     public function actionCreate()
     {
-        // create product category
-    }
-
-    protected function insertCategory()
-    {
-        $categories = require(__DIR__ . '/sources/product_category.php');
         $command = $this->db->createCommand();
-        $command->delete('{{%category}}');
-        foreach ($categories as $category) {
-            try {
-                $command->insert('{{%category}}', [
-                    'id' => $category[0],
-                    'code' => sprintf('%04d', $category[0]),
-                    'name' => $category[1],
-                ]);
-            } catch (\Exception $exc) {
-                
-            }
+        $faker = $this->generator;
+        $now = new Expression('NOW()');
+        
+        //*
+        // orgn
+        echo "\nInsert data orgn\n";
+        Console::startProgress(0, 1);
+        $command->delete('{{%orgn}}')->execute();
+        for ($i = 0; $i < 1; $i++) {
+            $command->insert('{{%orgn}}', [
+                'id' => $i + 1,
+                'code' => $i * 1000 + 1000,
+                'name' => $faker->companySuffix,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ])->execute();
+            Console::updateProgress($i+1, 1);
         }
-    }
+        Console::endProgress();
 
-    private function internalGenerateRow($_template_, $_params_ = [])
-    {
-        extract($_params_, EXTR_OVERWRITE);
-        return require($_template_);
+        // branch
+        echo "\nInsert data branch\n";
+        Console::startProgress(0, 4);
+        $command->delete('{{%branch}}')->execute();
+        for ($i = 0; $i < 4; $i++) {
+            $command->insert('{{%branch}}', [
+                'id' => $i + 1,
+                'orgn_id' => 1,
+                'code' => 1100 + 100 * $i,
+                'name' => $faker->city,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ])->execute();
+            Console::updateProgress($i+1, 4);
+        }
+        Console::endProgress();
+
+        // warehouse
+        echo "\nInsert data warehouse\n";
+        Console::startProgress(0, 8);
+        $command->delete('{{%warehouse}}')->execute();
+        for ($i = 0; $i < 8; $i++) {
+            $b_id = floor($i / 2) + 1;
+            $w_id = $i % 2 + 1;
+            $command->insert('{{%warehouse}}', [
+                'id' => $i + 1,
+                'branch_id' => $b_id,
+                'code' => 1000 + 100 * $b_id + $w_id,
+                'name' => $faker->streetName,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ])->execute();
+            Console::updateProgress($i+1, 8);
+        }
+        Console::endProgress();
+
+        // create product category
+        echo "Insert data product category\n";
+        $rows = require(__DIR__ . '/sources/product_category.php');
+        $total = count($rows);
+        $command->delete('{{%category}}')->execute();
+        foreach ($rows as $i => $row) {
+            $command->insert('{{%category}}', [
+                'id' => $row[0],
+                'code' => sprintf('%04d', $row[0]),
+                'name' => $row[1],
+                'created_at' => $now,
+                'updated_at' => $now,
+            ])->execute();
+            Console::updateProgress($i+1, $total);
+        }
+        Console::endProgress();
+
+        // product group
+        echo "Insert data product group\n";
+        $rows = require(__DIR__ . '/sources/product_group.php');
+        $total = count($rows);
+        Console::startProgress(0, $total);
+        $command->delete('{{%product_group}}')->execute();
+        foreach ($rows as $i => $row) {
+            $command->insert('{{%product_group}}', [
+                'id' => $row[0],
+                'code' => sprintf('%04d', $row[0]),
+                'name' => $row[1],
+                'created_at' => $now,
+                'updated_at' => $now,
+            ])->execute();
+            Console::updateProgress($i+1, 1);
+        }
+        Console::endProgress();
+
+        // product
+        echo "Insert data product\n";
+        $rows = require(__DIR__ . '/sources/product.php');
+        $total = count($rows);
+        Console::startProgress(0, $total);
+        $command->delete('{{%product}}')->execute();
+        foreach ($rows as $i => $row) {
+            $command->insert('{{%product}}', [
+                'id' => $row[0],
+                'group_id' => $row[1],
+                'category_id' => $row[2],
+                'code' => $faker->ean13,
+                'name' => $row[3],
+                'status' => 10,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ])->execute();
+            Console::updateProgress($i+1, $total);
+        }
+        Console::endProgress();
+
+        // barcode
+        echo "Insert data barcode product\n";
+        $rows = require(__DIR__ . '/sources/product.php');
+        $total = count($rows);
+        Console::startProgress(0, $total);
+        $command->delete('{{%product_child}}')->execute();
+        foreach ($rows as $i => $row) {
+            for ($j = 0; $j < 3; $j++) {
+                try {
+                    $command->insert('{{%product_child}}', [
+                        'barcode' => $faker->ean13,
+                        'product_id' => $row[0],
+                        'created_at' => $now,
+                        'updated_at' => $now,
+                    ])->execute();
+                } catch (\Exception $exc) {
+                    
+                }
+            }
+            Console::updateProgress($i+1, $total);
+        }
+        Console::endProgress();
+
+        // uom
+        echo "Insert data uom\n";
+        $rows = require(__DIR__ . '/sources/uom.php');
+        $total = count($rows);
+        Console::startProgress(0, $total);
+        $command->delete('{{%uom}}')->execute();
+        foreach ($rows as $row) {
+            $command->insert('{{%uom}}', [
+                'id' => $row[0],
+                'code' => $row[1],
+                'name' => $row[2],
+                'created_at' => $now,
+                'updated_at' => $now,
+            ])->execute();
+            Console::updateProgress($i+1, $total);
+        }
+        Console::endProgress();
+
+        // product uom
+        echo "Insert data product uom\n";
+        $rows = require(__DIR__ . '/sources/uom.php');
+        $total = count($rows);
+        Console::startProgress(0, $total);
+        $command->delete('{{%product_uom}}')->execute();
+        $command->sql = "insert into product_uom(product_id,uom_id,isi,created_at,updated_at)\n"
+            . "select id,:uom_id,:isi,NOW(),NOW() from product";
+        foreach ($rows as $row) {
+            $command->bindValues([':uom_id' => $row[0], ':isi' => $row[3]])->execute();
+            Console::updateProgress($i+1, $total);
+        }
+        Console::endProgress();
+        // */
+        
+        // supplier
+        echo "Insert data supplier\n";
+        Console::startProgress(0, 10);
+        $command->delete('{{%supplier}}')->execute();
+        for ($i = 0; $i < 10; $i++) {
+            $command->insert('{{%supplier}}', [
+                'id' => $i + 1,
+                'code' => 6001 + $i,
+                'name' => $faker->company,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ])->execute();
+            Console::updateProgress($i+1, 10);
+        }
+        Console::endProgress();
+
+        // customer
+        echo "Insert data customer\n";
+        Console::startProgress(0, 10);
+        $command->delete('{{%customer}}')->execute();
+        $command->insert('{{%customer}}', [
+            'id' => 1,
+            'code' => 8000,
+            'name' => 'Reguler',
+            'status' => 10,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ])->execute();
+        for ($i = 1; $i < 10; $i++) {
+            $command->insert('{{%customer}}', [
+                'id' => $i + 1,
+                'code' => 8000 + $i,
+                'name' => $faker->name,
+                'status' => 10,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ])->execute();
+            Console::updateProgress($i+1, 10);
+        }
+        Console::endProgress();
     }
 
     /**
@@ -89,14 +277,5 @@ class SampleDataController extends \yii\console\Controller
             $this->_generator = \Faker\Factory::create(str_replace('-', '_', $language));
         }
         return $this->_generator;
-    }
-
-    protected function generateRow($template, $index)
-    {
-        $template = __DIR__ . '/' . $template . '.php';
-        return $this->internalGenerateRow($template, [
-                'faker' => $this->getGenerator(),
-                'index' => $index,
-        ]);
     }
 }
--- app/commands/sources/uom.php
+++ app/commands/sources/uom.php
@@ -1,6 +1,6 @@
 <?php
 
 return[
-    [1, 'Pcs', 'Pieces'],
-    [2, 'Dzn', 'Dozen']
+    [1, 'Pcs', 'Pieces', 1],
+    [2, 'Dzn', 'Dozen', 12]
 ];
