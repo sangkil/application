eb27303f99bd6b78743e57f5147f5a7f5c2bcaf8
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-05 21:40:59 +0700

    toolbar and other look n feel

--- app/views/admin/rule/_form.php
+++ app/views/admin/rule/_form.php
@@ -9,21 +9,15 @@ use yii\widgets\ActiveForm;
  * @var yii\widgets\ActiveForm $form
  */
 ?>
-
-<div class="auth-item-form">
+<div class="box box-warning auth-item-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'name')->textInput(['maxlength' => 64]) ?>
-
-    <?= $form->field($model, 'className')->textInput() ?>
-
-    <div class="form-group">
-        <?php
-        echo Html::submitButton($model->isNewRecord ? 'Create' : 'Update', [
-            'class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary'])
-        ?>
+    <div class="box-body">
+        <?= $form->field($model, 'name')->textInput(['maxlength' => 64]) ?>
+        <?= $form->field($model, 'className')->textInput() ?>
     </div>
-
-<?php ActiveForm::end(); ?>
+    <div class="box-footer">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+    <?php ActiveForm::end(); ?>
 </div>
--- app/views/admin/rule/create.php
+++ app/views/admin/rule/create.php
@@ -1,5 +1,6 @@
 <?php
 
+use app\components\Toolbar;
 use yii\helpers\Html;
 
 /**
@@ -11,12 +12,18 @@ $this->title = 'Create Rule';
 $this->params['breadcrumbs'][] = ['label' => 'Rules', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="auth-item-create">
-
-	<h1><?= Html::encode($this->title) ?></h1>
-
-	<?php echo $this->render('_form', [
+<div class="col-lg-8 orgn-create">
+    <?php
+    echo Toolbar::widget(['items' => [
+            //['label' => 'Create', 'url' => ['create'], 'icon' => 'fa fa-plus-square', 'linkOptions' => ['class' => 'btn btn-success btn-sm']],
+            //['label' => 'Detail', 'url' => ['view', 'id' => $model->id],'icon' => 'fa fa-search', 'linkOptions' => ['class' => 'btn bg-navy btn-sm']],
+            //['label' => 'Update', 'url' => ['update', 'id' => $model->id],'icon' => 'fa fa-pencil', 'linkOptions' => ['class' => 'btn btn-warning btn-sm']],
+            //['label' => 'Delete', 'url' => ['delete', 'id' => $model->id],'icon' => 'fa fa-trash-o', 'linkOptions' => ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]],
+            ['label' => 'List', 'url' => ['index'],'icon' => 'fa fa-list', 'linkOptions' => ['class' => 'btn btn-info btn-sm']]
+    ]]);
+    ?>
+    <?= $this->render('_form', [
         'model' => $model,
-    ]); ?>
+    ]) ?>
 
 </div>
