e39860d2f6c65c445facde5a2864bf826b3af27b
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-05 21:30:34 +0700

    toolbar and other look n feel

--- app/views/admin/rule/index.php
+++ app/views/admin/rule/index.php
@@ -12,28 +12,34 @@ use yii\widgets\Pjax;
 $this->title = 'Rules';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="role-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Create Rule', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php
-    Pjax::begin([
-        'enablePushState'=>false,
-    ]);
-    echo GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            'name',
-            ['class' => 'yii\grid\ActionColumn',],
-        ],
-    ]);
-    Pjax::end();
+<div class="col-lg-12 rule-index">
+    <?=
+    Toolbar::widget(['items' => [
+            ['label' => 'Create', 'url' => ['create'], 'icon' => 'fa fa-plus-square', 'linkOptions' => ['class' => 'btn btn-success btn-sm']],
+        //['label' => 'Detail', 'url' => ['view', 'id' => $model->id], 'icon' => 'fa fa-search', 'linkOptions' => ['class' => 'btn bg-navy btn-sm']],
+        //['label' => 'Update', 'url' => ['update', 'id' => $model->id],'icon' => 'fa fa-pencil', 'linkOptions' => ['class' => 'btn btn-warning btn-sm']],
+        //['label' => 'Delete', 'url' => ['delete', 'id' => $model->id], 'icon' => 'fa fa-trash-o', 'linkOptions' => ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]],
+        //['label' => 'List', 'url' => ['index'], 'icon' => 'fa fa-list', 'linkOptions' => ['class' => 'btn btn-info btn-sm']]
+    ]]);
     ?>
-
+    <div class="box box-info">
+        <div class="box-body no-padding">
+            <?php
+            Pjax::begin([
+                'enablePushState' => false,
+            ]);
+            echo GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'layout' => "{items}\n{pager}",
+                'columns' => [
+                    ['class' => 'yii\grid\SerialColumn'],
+                    'name',
+                    ['class' => 'yii\grid\ActionColumn',],
+                ],
+            ]);
+            Pjax::end();
+            ?>
+        </div>
+    </div>
 </div>
