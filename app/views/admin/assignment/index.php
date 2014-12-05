171771936d67a05475eb87fd2b803543a5bb86a6
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-05 21:20:58 +0700

    toolbar and other look n feel

--- app/views/admin/assignment/index.php
+++ app/views/admin/assignment/index.php
@@ -32,6 +32,7 @@ $this->params['breadcrumbs'][] = $this->title;
             echo GridView::widget([
                 'dataProvider' => $dataProvider,
                 'filterModel' => $searchModel,
+                'layout' => "{items}\n{pager}",
                 'columns' => [
                     ['class' => 'yii\grid\SerialColumn'],
                     [
