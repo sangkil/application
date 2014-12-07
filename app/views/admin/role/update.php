c8744fa98ae98befd6bcf36df60ce10efe7edefc
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-07 18:20:05 +0700

    toolbar and other look n feel

--- app/views/admin/role/update.php
+++ app/views/admin/role/update.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use app\components\Toolbar;
 
 /**
  * @var yii\web\View $this
@@ -12,9 +13,16 @@ $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="auth-item-update">
-
-	<h1><?= Html::encode($this->title) ?></h1>
-	<?php
+    <?=
+    Toolbar::widget(['items' => [
+            ['label' => 'Create', 'url' => ['create'], 'icon' => 'fa fa-plus-square', 'linkOptions' => ['class' => 'btn btn-success btn-sm']],
+            ['label' => 'Detail', 'url' => ['view', 'id' => $model->name], 'icon' => 'fa fa-search', 'linkOptions' => ['class' => 'btn bg-navy btn-sm']],
+            //['label' => 'Update', 'url' => ['update', 'id' => $model->id],'icon' => 'fa fa-pencil', 'linkOptions' => ['class' => 'btn btn-warning btn-sm']],
+            ['label' => 'Delete', 'url' => ['delete', 'id' => $model->name], 'icon' => 'fa fa-trash-o', 'linkOptions' => ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]],
+            ['label' => 'List', 'url' => ['index'], 'icon' => 'fa fa-list', 'linkOptions' => ['class' => 'btn btn-info btn-sm']]
+    ]]);
+    ?>
+    <?php
     echo $this->render('_form', [
         'model' => $model,
     ]);
