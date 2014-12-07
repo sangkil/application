79adef001a8ad79f704cf7b4301465fcc15f3689
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-07 18:11:31 +0700

    toolbar and other look n feel

--- app/views/admin/assignment/view.php
+++ app/views/admin/assignment/view.php
@@ -9,60 +9,57 @@ use app\components\Toolbar;
 $this->title = 'Assignments for ' . $model->{$usernameField};
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="menu-view">
-    <div class="col-lg-12">
-        <?= ''//Html::a('Users', ['index'], ['class'=>'btn btn-success']) ?>
-<!--    <h1>User: <?= ''//$model->{$usernameField}    ?></h1>-->
-        <?=
-        Toolbar::widget(['items' => [
-                ['label' => 'Users', 'url' => ['index'], 'icon' => 'fa fa-list', 'linkOptions' => ['class' => 'btn btn-success btn-sm']]
-        ]]);
-        ?>        
-    </div>
-    <div class="col-lg-5">
-        <div class="box box-info menu-view">
-            <div class="box-header">
-                Avaliable:
-                <?php
-                echo Html::textInput('search_av', '', ['class' => 'role-search', 'data-target' => 'avaliable']) . '<br>';
-                ?>
-            </div>
-            <div class="box-body no-padding">
-                <?php
-                echo Html::listBox('roles', '', $avaliable, [
-                    'id' => 'avaliable',
-                    'multiple' => true,
-                    'size' => 20,
-                    'style' => 'width:100%']);
-                ?>
-            </div>
+<div class="col-lg-12 menu-view">
+    <?= ''//Html::a('Users', ['index'], ['class'=>'btn btn-success']) ?>
+<!--    <h1>User: <?= ''//$model->{$usernameField}     ?></h1>-->
+    <?=
+    Toolbar::widget(['items' => [
+            ['label' => 'Users', 'url' => ['index'], 'icon' => 'fa fa-list', 'linkOptions' => ['class' => 'btn btn-success btn-sm']]
+    ]]);
+    ?>        
+</div>
+<div class="col-lg-5">
+    <div class="box box-info menu-view">
+        <div class="box-header">
+            Avaliable:
+            <?php
+            echo Html::textInput('search_av', '', ['class' => 'role-search', 'data-target' => 'avaliable']) . '<br>';
+            ?>
+        </div>
+        <div class="box-body no-padding">
+            <?php
+            echo Html::listBox('roles', '', $avaliable, [
+                'id' => 'avaliable',
+                'multiple' => true,
+                'size' => 20,
+                'style' => 'width:100%']);
+            ?>
         </div>
     </div>
-    <div class="col-lg-2" style="text-align: center;">
-        <?php
-        echo Html::a('<<', '#', ['class' => 'btn btn-success', 'data-action' => 'delete']) . '&nbsp;';
-        echo Html::a('>>', '#', ['class' => 'btn btn-success', 'data-action' => 'assign']);
-        ?>
-    </div>
-    <div class="col-lg-5">
-        <div class="box box-info menu-view">
-            <div class="box-header">
-                Assigned:
-                <?php
-                echo Html::textInput('search_asgn', '', ['class' => 'role-search', 'data-target' => 'assigned']) . '<br>';
-                ?>
-            </div>
-            <div class="box-body no-padding">
-                <?php
-                echo Html::listBox('roles', '', $assigned, [
-                    'id' => 'assigned',
-                    'multiple' => true,
-                    'size' => 20,
-                    'style' => 'width:100%']);
-                ?>
-            </div>
+</div>
+<div class="col-lg-2" style="text-align: center;">
+    <?php
+    echo Html::a('<<', '#', ['class' => 'btn btn-success', 'data-action' => 'delete']) . '&nbsp;';
+    echo Html::a('>>', '#', ['class' => 'btn btn-success', 'data-action' => 'assign']);
+    ?>
+</div>
+<div class="col-lg-5">
+    <div class="box box-info menu-view">
+        <div class="box-header">
+            Assigned:
+            <?php
+            echo Html::textInput('search_asgn', '', ['class' => 'role-search', 'data-target' => 'assigned']) . '<br>';
+            ?>
         </div>
-
+        <div class="box-body no-padding">
+            <?php
+            echo Html::listBox('roles', '', $assigned, [
+                'id' => 'assigned',
+                'multiple' => true,
+                'size' => 20,
+                'style' => 'width:100%']);
+            ?>
+        </div>
     </div>
 </div>
 <?php
--- app/views/admin/role/view.php
+++ app/views/admin/role/view.php
@@ -12,73 +12,71 @@ $this->title = $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'Roles', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="auth-item-view">
-    <div class="col-lg-12">
-        <?=
-        Toolbar::widget(['items' => [
-                ['label' => 'Update', 'url' => ['update', 'id' => $model->name],'icon' => 'fa fa-pencil', 'linkOptions' => ['class' => 'btn btn-warning btn-sm']],
-                ['label' => 'Delete', 'url' => ['delete', 'id' => $model->name], 'icon' => 'fa fa-trash-o', 'linkOptions' => ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]],
-        ]]);
-        ?> 
+<div class="col-lg-12 auth-item-view">
+    <?=
+    Toolbar::widget(['items' => [
+            ['label' => 'Update', 'url' => ['update', 'id' => $model->name], 'icon' => 'fa fa-pencil', 'linkOptions' => ['class' => 'btn btn-warning btn-sm']],
+            ['label' => 'Delete', 'url' => ['delete', 'id' => $model->name], 'icon' => 'fa fa-trash-o', 'linkOptions' => ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]],
+    ]]);
+    ?> 
 
-        <div class="box box-info">
-            <div class="box-body no-padding">
-                <?php
-                echo DetailView::widget([
-                    'model' => $model,
-                    'attributes' => [
-                        'name',
-                        'description:ntext',
-                        'ruleName',
-                        'data:ntext',
-                    ],
-                ]);
-                ?> 
-            </div>
+    <div class="box box-info">
+        <div class="box-body no-padding">
+            <?php
+            echo DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'name',
+                    'description:ntext',
+                    'ruleName',
+                    'data:ntext',
+                ],
+            ]);
+            ?> 
         </div>
     </div>
-    <div class="col-lg-6">
-        <div class="box box-info menu-view">
-            <div class="box-header">
-                Avaliable:
-                <?php
-                echo Html::textInput('search_av', '', ['class' => 'role-search', 'data-target' => 'avaliable']) . '<br>';
-                ?>
-            </div>
-            <div class="box-body no-padding">
-                <?php
-                echo Html::listBox('roles', '', $avaliable, [
-                    'id' => 'avaliable',
-                    'multiple' => true,
-                    'size' => 20,
-                    'style' => 'width:100%']);
-                ?>
-            </div>
+</div>
+<div class="col-lg-6">
+    <div class="box box-info menu-view">
+        <div class="box-header">
+            Avaliable:
+            <?php
+            echo Html::textInput('search_av', '', ['class' => 'role-search', 'data-target' => 'avaliable']) . '<br>';
+            ?>
         </div>
-    </div>    
-    <div class="col-lg-2" style="text-align: center;">
-        <?php
-        echo Html::a('>>', '#', ['class' => 'btn btn-success', 'data-action' => 'assign']) . '<br>';
-        echo Html::a('<<', '#', ['class' => 'btn btn-success', 'data-action' => 'delete']) . '<br>';
-        ?>
+        <div class="box-body no-padding">
+            <?php
+            echo Html::listBox('roles', '', $avaliable, [
+                'id' => 'avaliable',
+                'multiple' => true,
+                'size' => 20,
+                'style' => 'width:100%']);
+            ?>
+        </div>
     </div>
-    <div class="col-lg-5">
-        <div class="box box-info menu-view">
-            <div class="box-header">
-                Assigned:
-                <?php
-                echo Html::textInput('search_asgn', '', ['class' => 'role-search', 'data-target' => 'assigned']) . '<br>';
-                ?>
-            </div>
-            <div class="box-body no-padding">
-                <?php
-                echo Html::listBox('roles', '', $assigned, [
-                    'id' => 'assigned',
-                    'multiple' => true,
-                    'size' => 20,
-                    'style' => 'width:100%']);
-                ?>
-            </div>
+</div>    
+<div class="col-lg-2" style="text-align: center;">
+    <?php
+    echo Html::a('>>', '#', ['class' => 'btn btn-success', 'data-action' => 'assign']) . '<br>';
+    echo Html::a('<<', '#', ['class' => 'btn btn-success', 'data-action' => 'delete']) . '<br>';
+    ?>
+</div>
+<div class="col-lg-5">
+    <div class="box box-info menu-view">
+        <div class="box-header">
+            Assigned:
+            <?php
+            echo Html::textInput('search_asgn', '', ['class' => 'role-search', 'data-target' => 'assigned']) . '<br>';
+            ?>
+        </div>
+        <div class="box-body no-padding">
+            <?php
+            echo Html::listBox('roles', '', $assigned, [
+                'id' => 'assigned',
+                'multiple' => true,
+                'size' => 20,
+                'style' => 'width:100%']);
+            ?>
         </div>
     </div>
 </div>
