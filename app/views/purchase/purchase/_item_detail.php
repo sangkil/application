294b7df11499acd69a22992b3deedc381d38af7f
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-08 19:44:52 +0700

    toolbar and other look n feel

--- app/views/purchase/purchase/_item_detail.php
+++ app/views/purchase/purchase/_item_detail.php
@@ -6,16 +6,20 @@ use yii\helpers\Html;
 /* @var $model app\models\purchase\PurchaseDtl */
 /* @var $key string */
 ?>
-<div class="table-cell"><span class="product"></span></div>
-<div class="table-cell"><?= Html::activeTextInput($model, "[$key]qty", ['data-field' => 'qty', 'size' => 5, 'id' => false, 'required' => true]) ?>
-    <?= Html::activeDropDownList($model, "[$key]uom_id", [], ['data-field' => 'uom_id', 'id' => false]) ?></div>
-<div class="table-cell">
+<div class="table-cell" style="width: 20%;">
+    <span class="product"></span>
+</div>
+<div class="table-cell" style="width: 40%;">
+    <?= Html::activeTextInput($model, "[$key]qty", ['data-field' => 'qty', 'size' => 5, 'id' => false, 'required' => true]) ?>
+    <?= Html::activeDropDownList($model, "[$key]uom_id", [], ['data-field' => 'uom_id', 'id' => false]) ?>
+</div>
+<div class="table-cell" style="width: 15%;">
     <?= Html::activeTextInput($model, "[$key]price", ['data-field' => 'price', 'size' => 16, 'id' => false, 'required' => true]) ?>
 </div>
-<div class="table-cell">
+<div class="table-cell" style="width: 20%;">
     <input type="hidden" data-field="total_price"><span class="total-price"></span>
 </div>
-<div class="table-cell">
+<div class="table-cell" style="width: 5%;">
     <a data-action="delete" title="Delete" href="#"><span class="glyphicon glyphicon-trash"></span></a>
     <?= Html::activeHiddenInput($model, "[$key]product_id", ['data-field' => 'product_id', 'id' => false]) ?>
     <?= Html::activeHiddenInput($model, "[$key]uom_id", ['data-field' => 'sel_uom_id', 'id' => false, 'name' => false]) ?>
