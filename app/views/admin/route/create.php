32e76db456805c9663d18e1120190f983d0bc2c0
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-08 09:21:19 +0700

    toolbar and other look n feel

--- app/views/admin/route/create.php
+++ app/views/admin/route/create.php
@@ -8,22 +8,35 @@ use yii\widgets\ActiveForm;
  * @var mdm\admin\models\Route $model
  * @var ActiveForm $form
  */
-
 $this->title = 'Create Route';
 $this->params['breadcrumbs'][] = ['label' => 'Routes', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<h1>Create Route</h1>
 
-<div class="create">
-
-	<?php $form = ActiveForm::begin(); ?>
-
-		<?= $form->field($model, 'route') ?>
-
-		<div class="form-group">
-			<?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
-		</div>
-	<?php ActiveForm::end(); ?>
+<div class="col-lg-12 create">
+    <?php
+    echo Toolbar::widget(['items' => [
+            //['label' => 'Create', 'url' => ['create'], 'icon' => 'fa fa-plus-square', 'linkOptions' => ['class' => 'btn btn-success btn-sm']],
+            //['label' => 'Detail', 'url' => ['view', 'id' => $model->id],'icon' => 'fa fa-search', 'linkOptions' => ['class' => 'btn bg-navy btn-sm']],
+            //['label' => 'Update', 'url' => ['update', 'id' => $model->id],'icon' => 'fa fa-pencil', 'linkOptions' => ['class' => 'btn btn-warning btn-sm']],
+            //['label' => 'Delete', 'url' => ['delete', 'id' => $model->id],'icon' => 'fa fa-trash-o', 'linkOptions' => ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]],
+            ['label' => 'List', 'url' => ['index'], 'icon' => 'fa fa-list', 'linkOptions' => ['class' => 'btn btn-info btn-sm']]
+    ]]);
+    ?>
+    <?php
+    echo $this->render('_form', [
+        'model' => $model,
+    ]);
+    ?>
+    <?php $form = ActiveForm::begin(); ?>
+    <div class="box box-info">
+        <div class="box-body">
+            <?= $form->field($model, 'route') ?>
+        </div>
+        <div class="box-footer">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        </div>
+    </div>
+    <?php ActiveForm::end(); ?>
 
 </div><!-- create -->
