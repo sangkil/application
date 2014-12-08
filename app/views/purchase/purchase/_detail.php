d49ed957ccbaecf264793e19ca5ef34f08a00709
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-08 22:05:09 +0700

    toolbar and other look n feel

--- app/views/purchase/purchase/_detail.php
+++ app/views/purchase/purchase/_detail.php
@@ -23,33 +23,37 @@ use mdm\widgets\TabularInput;
                         'source' => new JsExpression('biz.master.sourceProduct'),
                         'select' => new JsExpression('biz.purchase.onProductSelect'),
                         'delay' => 100,
-                    ],'options' => ['class'=>'form-control'],
+                    ], 'options' => ['class' => 'form-control'],
                 ]);
                 ?>
             </div>
             <div class="col-xs-4">
                 Item Discount:
-                <?= Html::activeTextInput($model, 'discount', [ 'id' => 'item-discount', 'class'=>'form-control']); ?>
+                <?= Html::activeTextInput($model, 'discount', [ 'id' => 'item-discount', 'class' => 'form-control']); ?>
             </div>
         </div> 
         <table class="tabular table-striped">
-        <?=
-        TabularInput::widget([
-            'id' => 'detail-grid',
-            'allModels' => $details,
-            'modelClass' => PurchaseDtl::className(),
-            'options' => ['tag'=>'tbody'],
-            'itemOptions' => ['tag' => 'tr'],
-            'itemView' => '_item_detail',
-            'clientOptions' => [
-            ]
-        ])
-        ?>
+            <?=
+            TabularInput::widget([
+                'id' => 'detail-grid',
+                'allModels' => $details,
+                'modelClass' => PurchaseDtl::className(),
+                'options' => ['tag' => 'tbody'],
+                'itemOptions' => ['tag' => 'tr'],
+                'itemView' => '_item_detail',
+                'clientOptions' => [
+                ]
+            ])
+            ?>
         </table>
     </div>
-    <div class="box-footer">    
-        <?= Html::activeHiddenInput($model, 'value', ['id' => 'purchase-value']); ?>
-        <h4 id="bfore" style="display: none;">Rp <span id="purchase-val">0</span>-<span id="disc-val">0</span></h4>
-        <h2>Rp <span id="total-price"></span></h2>
+    <div class="box-footer"> 
+        <div class="col-lg-9">
+            <?= Html::activeHiddenInput($model, 'value', ['id' => 'purchase-value']); ?>
+            <h4 id="bfore" style="display: none;">Rp <span id="purchase-val">0</span>-<span id="disc-val">0</span></h4>         
+        </div>
+        <div class="col-lg-3">            
+            <h2>Rp <span id="total-price"></span></h2>
+        </div>
     </div>
 </div>  
