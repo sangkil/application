a097e9509af6faded6b1df7f833cd5e7adeb2de0
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-09 16:08:26 +0700

    toolbar and other look n feel

--- app/views/purchase/purchase/view.php
+++ app/views/purchase/purchase/view.php
@@ -14,39 +14,53 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div class="col-lg-12">
-    <div class="box box-primary">
-        <div class="box-body no-padding">
-            <?php
-            echo DetailView::widget([
-                'options' => ['class' => 'table table-striped detail-view'],
-                'model' => $model,
-                'attributes' => [
-                    'number',
-                    'nmSupplier',
-                    'Date',
-                    'value',
-                    'nmStatus',
-                ],
-            ]);
-            ?>
+    <div class="nav-tabs-custom">
+        <ul class="nav nav-tabs">
+            <li class="active"><a href="#tab_1-1" data-toggle="tab">Header</a></li>
+            <li><a href="#tab_2-2" data-toggle="tab"><i class="fa fa-money"></i> Finance & Costing</a></li>
+        </ul>
+        <div class="tab-content">
+            <div class="tab-pane active" id="tab_1-1" style="min-height: 10em;">
+                <div class="box box-primary">
+                    <div class="box-body no-padding">
+                        <?php
+                        echo DetailView::widget([
+                            'options' => ['class' => 'table table-striped detail-view'],
+                            'model' => $model,
+                            'attributes' => [
+                                'number',
+                                'nmSupplier',
+                                'Date',
+                                'value',
+                                'nmStatus',
+                            ],
+                        ]);
+                        ?>
+                    </div>
+                    <div class="box-footer">
+                        <?php
+                        if ($model->status == Purchase::STATUS_DRAFT) {
+                            echo Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) . ' ';
+                            echo Html::a('Delete', ['delete', 'id' => $model->id], [
+                                'class' => 'btn btn-danger',
+                                'data-confirm' => Yii::t('app', 'Are you sure to delete this item?'),
+                                'data-method' => 'post',
+                            ]) . ' ';
+                        }
+                        echo Html::a('Receive', ['receive', 'id' => $model->id], [
+                            'class' => 'btn btn-success',
+                        ]);
+                        ?>
+                    </div>
+                </div> 
+            </div>
         </div>
-        <div class="box-footer">
-            <?php
-            if ($model->status == Purchase::STATUS_DRAFT) {
-                echo Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) . ' ';
-                echo Html::a('Delete', ['delete', 'id' => $model->id], [
-                    'class' => 'btn btn-danger',
-                    'data-confirm' => Yii::t('app', 'Are you sure to delete this item?'),
-                    'data-method' => 'post',
-                ]) . ' ';
-            }
-            echo Html::a('Receive', ['receive', 'id' => $model->id], [
-                'class' => 'btn btn-success',
-            ]);
-            ?>
+        <div class="tab-pane" id="tab_2-2">
+            Shipping Cost, dll.
         </div>
-    </div> 
-</div>
+    </div>
+</div>   
+
 <div class="col-lg-12">
     <div class="box box-info">
         <div class="box-body no-padding">
