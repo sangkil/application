4d149e51492927b68e49b4cedb7e7612c90e118a
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-10 10:35:54 +0700

    toolbar and other look n feel

--- app/views/site/error.php
+++ app/views/site/error.php
@@ -11,7 +11,7 @@ use yii\base\Exception;
 $this->title = $name;
 ?>
 <div class="error-page">
-    <h2 class="headline text-info"> <?php echo ErrorException::code ?></h2>
+    <h2 class="headline text-info"> <?php echo Exception::code ?></h2>
     <div class="error-content">
         <h3><i class="fa fa-warning text-yellow"></i> Oops! <?= nl2br(Html::encode($message)) ?></h3>
         <p>
