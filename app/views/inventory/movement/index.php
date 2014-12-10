91a3109e7ded241c24edd54e2dea5401a4ab8cad
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-10 09:53:03 +0700

    toolbar and other look n feel

--- app/views/inventory/movement/index.php
+++ app/views/inventory/movement/index.php
@@ -13,57 +13,65 @@ $this->title = 'Good Movements';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="good-movement-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Good Movement', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-    <?php
-    $filterRef = [];
-    foreach (Configs::movement() as $key => $value) {
-        $filterRef[$key] = isset($value['name']) ? $value['name'] : $key;
-    }
-    ?>
-    <?=
-    GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-            'number',
-            'Date',
-            [
-                'attribute' => 'type',
-                'value' => 'nmType',
-                'filter' => [
-                    GoodsMovement::TYPE_RECEIVE => 'Receive',
-                    GoodsMovement::TYPE_ISSUE => 'Issue',
-                ]
-            ],
-            [
-                'attribute' => 'reff_type',
-                'value' => 'nmReffType',
-                'filter' => $filterRef,
-            ],
-            [
-                'label' => 'Reference',
-                'value' => 'reffLink',
-                'format' => 'raw',
-            ],
-            [
-                'attribute'=>'status',
-                'value'=>'nmStatus',
-                'filter'=>[
-                    GoodsMovement::STATUS_DRAFT => 'Draft',
-                    GoodsMovement::STATUS_APPLIED => 'Applied',
-                    GoodsMovement::STATUS_INVOICED => 'Invoiced',
-                ]
-            ],
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]);
-    ?>
-
-</div>
+    <div class="purchase-index">
+        <?php
+        echo Toolbar::widget(['items' => [
+                ['label' => 'Create', 'url' => ['create'], 'icon' => 'fa fa-plus-square', 'linkOptions' => ['class' => 'btn btn-success btn-sm']],
+            //['label' => 'Detail', 'url' => ['view', 'id' => $model->id],'icon' => 'fa fa-search', 'linkOptions' => ['class' => 'btn bg-navy btn-sm']],
+            //['label' => 'Update', 'url' => ['update', 'id' => $model->id],'icon' => 'fa fa-pencil', 'linkOptions' => ['class' => 'btn btn-warning btn-sm']],
+            //['label' => 'Delete', 'url' => ['delete', 'id' => $model->id],'icon' => 'fa fa-trash-o', 'linkOptions' => ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]],
+            //['label' => 'List', 'url' => ['index'],'icon' => 'fa fa-list', 'linkOptions' => ['class' => 'btn btn-info btn-sm']]
+        ]]);
+        ?>
+        <div class="box box-info">
+            <div class="box-body no-padding">
+                <?php
+                $filterRef = [];
+                foreach (Configs::movement() as $key => $value) {
+                    $filterRef[$key] = isset($value['name']) ? $value['name'] : $key;
+                }
+                ?>
+                <?=
+                GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'filterModel' => $searchModel,
+                    'layout' => "{items}\n{pager}",
+                    'tableOptions' => ['class' => 'table table-striped'],
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
+                        'number',
+                        'Date',
+                        [
+                            'attribute' => 'type',
+                            'value' => 'nmType',
+                            'filter' => [
+                                GoodsMovement::TYPE_RECEIVE => 'Receive',
+                                GoodsMovement::TYPE_ISSUE => 'Issue',
+                            ]
+                        ],
+                        [
+                            'attribute' => 'reff_type',
+                            'value' => 'nmReffType',
+                            'filter' => $filterRef,
+                        ],
+                        [
+                            'label' => 'Reference',
+                            'value' => 'reffLink',
+                            'format' => 'raw',
+                        ],
+                        [
+                            'attribute' => 'status',
+                            'value' => 'nmStatus',
+                            'filter' => [
+                                GoodsMovement::STATUS_DRAFT => 'Draft',
+                                GoodsMovement::STATUS_APPLIED => 'Applied',
+                                GoodsMovement::STATUS_INVOICED => 'Invoiced',
+                            ]
+                        ],
+                        ['class' => 'yii\grid\ActionColumn'],
+                    ],
+                ]);
+                ?>
+            </div>
+        </div>
+    </div>
