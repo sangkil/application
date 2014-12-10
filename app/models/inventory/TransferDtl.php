4675213b0fb9f938762ce6d9f688275584006d9b
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-10 12:04:06 +0700

    toolbar and other look n feel

---
+++ app/models/inventory/TransferDtl.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\models\inventory;
+
+use Yii;
+use app\models\master\Product;
+use app\models\master\Uom;
+
+/**
+ * GoodsMovementDtl
+ *
+ * @property Product $product
+ * @property Uom $uom
+ * 
+ * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ * @since 1.0
+ */
+class TransferDtl extends \biz\core\inventory\models\TransferDtl
+{
+
+    public function getProduct()
+    {
+        return $this->hasOne(Product::className(), ['id' => 'product_id']);
+    }
+    
+    public function getUom()
+    {
+        return $this->hasOne(Uom::className(), ['id' => 'uom_id']);
+    }
+}
