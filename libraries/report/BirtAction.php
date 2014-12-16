550327f46488e11bad8c3fe25b51dfe75271a7ed
mdmunir <misbahuldmunir@gmail.com>
2014-12-16 21:12:50 +0700

    Birt report

--- libraries/report/BirtAction.php
+++ libraries/report/BirtAction.php
@@ -18,21 +18,38 @@ class BirtAction extends \yii\base\Action
      * @var BirtReport 
      */
     public $report;
+    public $name;
+    public $outputType = 'html';
 
     /**
-     * @var Response 
+     * @inheritdoc
      */
-    public $response = 'response';
-
     public function init()
     {
         $this->report = Instance::ensure($this->report, BirtReport::className());
-        $this->response = Instance::ensure($this->response, Response::className());
     }
 
+    
     public function run()
     {
-        $queryParams = Yii::$app->request->getQueryParams();
-        
+        /* @var $request \yii\web\Request */
+        /* @var $response \yii\web\Response */
+        $request = Yii::$app->request;
+        $response = Yii::$app->response;
+        $reportName = $request->getQueryParam('name', $this->name);
+        $outputType = $request->getQueryParam('outputType', $this->outputType);
+        $params = $request->getQueryParam('params', []);
+
+        $return = $this->report->renderReport($reportName, [
+            'params' => $params
+            ], $outputType);
+
+        if ($outputType != BirtReport::OUTPUT_TYPE_HTML) {
+            $response->format = Response::FORMAT_RAW;
+            $fileName = explode('.', $reportName, 1)[0] . '.' . $outputType;
+            $mimeType = \yii\helpers\FileHelper::getMimeTypeByExtension($fileName);
+            $response->setDownloadHeaders($fileName, $mimeType);
+        }
+        return $return;
     }
 }
