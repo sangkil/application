b34a2a0c74548dbf13391bc28bd4b3b70225b567
mujib.masyhudi <mujib.masyhudi@gmail.com>
2014-12-30 16:07:05 +0700

    price display modification

--- app/views/master/price/index.php
+++ app/views/master/price/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php
     echo Toolbar::widget(['items' => [
             ['label' => 'Create', 'url' => ['create'], 'icon' => 'fa fa-plus-square', 'linkOptions' => ['class' => 'btn btn-success btn-sm']],
-            ['label' => 'Create By PO', 'url' => ['createByPO'], 'icon' => 'fa fa-shopping-cart', 'linkOptions' => ['class' => 'btn btn-primary btn-sm']],
+            ['label' => 'Create By PO', 'url' => ['create-by-po'], 'icon' => 'fa fa-shopping-cart', 'linkOptions' => ['class' => 'btn btn-primary btn-sm']],
         //['label' => 'Detail', 'url' => ['view', 'id' => $model->id],'icon' => 'fa fa-search', 'linkOptions' => ['class' => 'btn bg-navy btn-sm']],
         //['label' => 'Update', 'url' => ['update', 'id' => $model->id],'icon' => 'fa fa-pencil', 'linkOptions' => ['class' => 'btn btn-warning btn-sm']],
         //['label' => 'Delete', 'url' => ['delete', 'id' => $model->id],'icon' => 'fa fa-trash-o', 'linkOptions' => ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]],
