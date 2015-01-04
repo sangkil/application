79eef9234585157feeb770ea5d3f77d46c64e8fc
mdmunir <misbahuldmunir@gmail.com>
2015-01-04 21:14:22 +0700

    change price by po

--- app/controllers/master/PriceController.php
+++ app/controllers/master/PriceController.php
@@ -8,12 +8,17 @@ use app\models\master\searchs\Price as PriceSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use app\models\purchase\Purchase;
+use app\models\master\ProductPrice;
+use app\models\master\PriceCategory;
+use app\models\purchase\searchs\Purchase as PurchaseSearch;
 
 /**
  * PriceController implements the CRUD actions for Price model.
  */
 class PriceController extends Controller
 {
+
     public function behaviors()
     {
         return [
@@ -36,8 +41,8 @@ class PriceController extends Controller
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
+                'searchModel' => $searchModel,
+                'dataProvider' => $dataProvider,
         ]);
     }
 
@@ -50,7 +55,7 @@ class PriceController extends Controller
     public function actionView($product_id, $price_category_id)
     {
         return $this->render('view', [
-            'model' => $this->findModel($product_id, $price_category_id),
+                'model' => $this->findModel($product_id, $price_category_id),
         ]);
     }
 
@@ -67,27 +72,72 @@ class PriceController extends Controller
             return $this->redirect(['view', 'product_id' => $model->product_id, 'price_category_id' => $model->price_category_id]);
         } else {
             return $this->render('create', [
-                'model' => $model,
+                    'model' => $model,
             ]);
         }
     }
-    
+
+    public function actionListPo()
+    {
+        $searchModel = new PurchaseSearch([
+            'status'=>  Purchase::STATUS_PROCESS
+        ]);
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        $dataProvider->getSort()->defaultOrder = [
+            'date' => SORT_DESC,
+            'number'=>SORT_DESC
+        ];
+        return $this->render('list_po', [
+                'searchModel' => $searchModel,
+                'dataProvider' => $dataProvider,
+        ]);
+    }
+
     /**
      * Creates a new Price model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
-    public function actionCreateByPo()
+    public function actionCreateByPo($id)
     {
-        $model = new Price();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'product_id' => $model->product_id, 'price_category_id' => $model->price_category_id]);
-        } else {
-            return $this->render('create_by_po', [
-                'model' => $model,
-            ]);
+        if (($purchase = Purchase::findOne($id)) === null) {
+            throw new NotFoundHttpException('The requested page does not exist.');
         }
+        $products = [];
+        foreach ($purchase->getPurchaseDtls()->with('product.category')->all() as $detail) {
+            $product = new ProductPrice([
+                'id' => $detail->product_id,
+                'name' => $detail->product->name,
+                'category' => $detail->product->category->name,
+                'price' => $detail->price,
+            ]);
+            $products[$detail->product_id] = $product;
+        }
+        if (ProductPrice::loadMultiple($products, Yii::$app->request->post()) && ProductPrice::validateMultiple($products)) {
+            $transaction = Yii::$app->db->beginTransaction();
+            try {
+                $success = true;
+                foreach ($products as $product) {
+                    if (!$product->save(false)) {
+                        $success = false;
+                        break;
+                    }
+                }
+                if ($success) {
+                    $transaction->commit();
+                } else {
+                    $transaction->rollBack();
+                }
+            } catch (\Exception $exc) {
+                $transaction->rollBack();
+                throw $exc;
+            }
+        }
+
+        return $this->render('create_by_po', [
+                'products' => $products,
+                'categories' => PriceCategory::find()->all()
+        ]);
     }
 
     /**
@@ -105,7 +155,7 @@ class PriceController extends Controller
             return $this->redirect(['view', 'product_id' => $model->product_id, 'price_category_id' => $model->price_category_id]);
         } else {
             return $this->render('update', [
-                'model' => $model,
+                    'model' => $model,
             ]);
         }
     }
