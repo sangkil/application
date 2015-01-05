8eb111dd243019b036ed19c01a23bc5fddc85db8
mujib.masyhudi <mujib.masyhudi@gmail.com>
2015-01-05 09:05:38 +0700

    price display modification

--- app/models/master/Price.php
+++ app/models/master/Price.php
@@ -8,24 +8,25 @@ namespace app\models\master;
  * @author Misbahul D Munir <misbahuldmunir@gmail.com>
  * @since 1.0
  */
-class Price extends \biz\core\master\models\Price
-{
+class Price extends \biz\core\master\models\Price {
+
     public $product_name;
-    
+
     /**
      * @inheritdoc
      */
-    public function rules()
-    {
-        return array_merge(parent::rules(),[[['product_name'], 'required']]);
+    public function rules() {
+        return array_merge(parent::rules(), [
+            [['product_name'], 'required'],
+            [['product_id', 'price_category_id'], 'unique', 'targetAttribute' => ['product_id', 'price_category_id']]
+        ]);
     }
-    
+
     /**
      * @inheritdoc
      */
-    public function attributeLabels()
-    {
-        return array_merge(parent::attributeLabels(), ['product_name'=>'Name of Product']);
+    public function attributeLabels() {
+        return array_merge(parent::attributeLabels(), ['product_name' => 'Product Name']);
     }
-            
+
 }
