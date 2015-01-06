fa25c9d2922f3a8b5b52b103b9e53cc462055668
mujib.masyhudi <mujib.masyhudi@gmail.com>
2015-01-06 09:24:29 +0700

    product report print

--- app/views/master/product/index.php
+++ app/views/master/product/index.php
@@ -15,13 +15,17 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php
     echo Toolbar::widget(['items' => [
             ['label' => 'Create', 'url' => ['create'], 'icon' => 'fa fa-plus-square', 'linkOptions' => ['class' => 'btn btn-success btn-sm']],
-            ['label' => '', 'url' => ['print-html'], 'icon' => 'fa fa-print', 'linkOptions' => ['class' => 'btn bg-navy btn-sm', 'target'=>'_blank']],
-            ['label' => '', 'url' => ['print-pdf'], 'icon' => 'fa fa-file-o', 'linkOptions' => ['class' => 'btn bg-navy btn-sm', 'target'=>'_blank']],
-            ['label' => '', 'url' => ['print-xsl'], 'icon' => 'fa fa-th', 'linkOptions' => ['class' => 'btn bg-navy btn-sm', 'target'=>'_blank']],
         //['label' => 'Detail', 'url' => ['view', 'id' => $model->id],'icon' => 'fa fa-search', 'linkOptions' => ['class' => 'btn bg-navy btn-sm']],
         //['label' => 'Update', 'url' => ['update', 'id' => $model->id],'icon' => 'fa fa-pencil', 'linkOptions' => ['class' => 'btn btn-warning btn-sm']],
         //['label' => 'Delete', 'url' => ['delete', 'id' => $model->id],'icon' => 'fa fa-trash-o', 'linkOptions' => ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]],
         //['label' => 'List', 'url' => ['index'],'icon' => 'fa fa-list', 'linkOptions' => ['class' => 'btn btn-info btn-sm']]
+    ]]);
+    ?>
+    <?php
+    echo Toolbar::widget(['items' => [
+            ['label' => '', 'url' => ['print-html'], 'icon' => 'fa fa-print', 'linkOptions' => ['class' => 'btn bg-maroon btn-sm', 'target'=>'_blank', 'title'=>'Html Print']],
+            ['label' => '', 'url' => ['print-pdf'], 'icon' => 'fa fa-file-o', 'linkOptions' => ['class' => 'btn bg-orange btn-sm', 'target'=>'_blank', 'title'=>'Export to Pdf']],
+            ['label' => '', 'url' => ['print-xsl'], 'icon' => 'fa fa-th', 'linkOptions' => ['class' => 'btn bg-olive btn-sm', 'target'=>'_blank', 'title'=>'Export to Excel']],
     ]]);
     ?>
     <div class="box box-info">
