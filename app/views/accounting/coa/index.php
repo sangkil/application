c60f75359c6bc57e03c13fba57bdbe98f217262e
mujib.masyhudi <mujib.masyhudi@gmail.com>
2015-01-06 15:48:43 +0700

    coa fixing display

--- app/views/accounting/coa/index.php
+++ app/views/accounting/coa/index.php
@@ -11,33 +11,47 @@ $this->title = 'Coas';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="coa-index">
+    <?php
+    echo Toolbar::widget(['items' => [
+            ['label' => 'Create', 'url' => ['create'], 'icon' => 'fa fa-plus-square', 'linkOptions' => ['class' => 'btn btn-success btn-sm']],
+        //['label' => 'Detail', 'url' => ['view', 'id' => $model->id],'icon' => 'fa fa-search', 'linkOptions' => ['class' => 'btn bg-navy btn-sm']],
+        //['label' => 'Update', 'url' => ['update', 'id' => $model->id],'icon' => 'fa fa-pencil', 'linkOptions' => ['class' => 'btn btn-warning btn-sm']],
+        //['label' => 'Delete', 'url' => ['delete', 'id' => $model->id],'icon' => 'fa fa-trash-o', 'linkOptions' => ['class' => 'btn btn-danger btn-sm', 'data' => ['confirm' => 'Are you sure you want to delete this item?', 'method' => 'post']]],
+        //['label' => 'List', 'url' => ['index'],'icon' => 'fa fa-list', 'linkOptions' => ['class' => 'btn btn-info btn-sm']]
+    ]]);
+    ?>
+    <?php
+    echo Toolbar::widget(['items' => [
+            ['label' => '', 'url' => ['print-html'], 'icon' => 'fa fa-print', 'linkOptions' => ['class' => 'btn bg-maroon btn-sm', 'target' => '_blank', 'title' => 'Html Print']],
+            ['label' => '', 'url' => ['print-pdf'], 'icon' => 'fa fa-file', 'linkOptions' => ['class' => 'btn bg-orange btn-sm', 'target' => '_blank', 'title' => 'Export to Pdf']],
+            ['label' => '', 'url' => ['print-xsl'], 'icon' => 'fa fa-table', 'linkOptions' => ['class' => 'btn bg-olive btn-sm', 'target' => '_blank', 'title' => 'Export to Excel']],
+    ]]);
+    ?>
+    <div class="box box-info">
+        <div class="box-body no-padding">
+            <?=
+            GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'layout'=>"{items}\n{pager}",
+                'tableOptions'=>['class'=>'table table-striped'],
+                'columns' => [
+                    ['class' => 'yii\grid\SerialColumn'],
+                    //'id',
+                    //'parent_id',
+                    'code',
+                    'name',
+                    'type',
+                    'normal_balance',
+                    // 'created_at',
+                    // 'created_by',
+                    // 'updated_at',
+                    // 'updated_by',
+                    ['class' => 'yii\grid\ActionColumn'],
+                ],
+            ]);
+            ?>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Coa', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'parent_id',
-            'code',
-            'name',
-            'type',
-            // 'normal_balance',
-            // 'created_at',
-            // 'created_by',
-            // 'updated_at',
-            // 'updated_by',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-
+        </div>
+    </div>
 </div>
