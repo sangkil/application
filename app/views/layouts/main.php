b713f4883e182f2c7ec83c19f2c0507efed02baf
mujib.masyhudi <mujib.masyhudi@gmail.com>
2015-03-02 09:57:02 +0700

    new look of adminLTE

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -19,21 +19,33 @@ $asset2 = app\assets\AddAsset::register($this);
     <head>
         <meta charset="<?= Yii::$app->charset ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <!-- Ionicons -->
+        <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
     </head>
-    <?php $this->beginBody() ?>
-    <body class="skin-blue">
-        <header class="header">
-            <?php echo $this->render('heading', ['baseurl' => $asset->baseUrl]); ?>
-        </header>
-        <div class="wrapper row-offcanvas row-offcanvas-left">
-            <aside class="right-side">
+    <?php $this->beginBody() ?> 
+    <body class="skin-blue layout-fixed sidebar-open">
+        <div class="wrapper">
+            <!-- header logo: style can be found in header.less -->
+            <header class="main-header">
+                <?php echo $this->render('heading2', ['baseurl' => $asset->baseUrl]); ?>
+            </header>
+            <!-- Left side column. contains the logo and sidebar -->
+            <aside class="main-sidebar">
+                <?php echo $this->render('sidebar2', ['baseurl' => $asset->baseUrl]); ?>
+            </aside>
+            <!-- Right side column. Contains the navbar and content of the page -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
                 <section class="content-header">
                     <h1>
-                        <?= '&nbsp;' . Html::encode($this->title) ?>
-                        <small><?php //echo \Yii::$app->controller->id . '-' . \Yii::$app->controller->action->id; ?></small>
+                        <?= Html::encode($this->title) ?>
+                        <small><?php echo \Yii::$app->controller->id . '-' . \Yii::$app->controller->action->id; ?></small>
                     </h1>
                     <?php
                     $breadcrumbs = isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [];
@@ -52,24 +64,31 @@ $asset2 = app\assets\AddAsset::register($this);
                     ])
                     ?>
                 </section>
+                <!-- Main content -->
                 <section class="content">
-                    <?= Alert::widget() ?>
-                    <?= $content ?>
-                </section>
-            </aside>            
-            <aside class="left-side sidebar-offcanvas">
-                <?php echo $this->render('sidebar', ['baseurl' => $asset->baseUrl]); ?>
-            </aside>
-        </div>
+                   <?= Alert::widget() ?>
+                   <?= $content ?>
+                </section><!-- /.content -->
+            </div><!-- /.content-wrapper -->
+            
+            <footer class="main-footer">
+                <div class="pull-right hidden-xs">
+                    <b>Version</b> 3.0
+                </div>
+                <strong>Copyright &copy; 2006-2015 <a href="http://sangkilsoft.com">SangkilSoft</a>.</strong> All rights reserved.
+            </footer>
 
-        <!--        <footer class="footer">
-                    <div class="container">
-                        <p class="pull-left">&copy; My Company <?= ''//date('Y')              ?></p>
-                        <p class="pull-right"><?= ''//Yii::powered()              ?></p>
-                    </div>
-                </footer>-->
-    </div>
+        </div><!-- ./wrapper -->
+
+        <!-- Pace 1.0.0 --
+        <script src="js/plugins/pace/pace.js" type="text/javascript"></script-->
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js" type="text/javascript"></script>
+        
+        <!-- AdminLTE for demo purposes -->
+        <!--    <script src="dist/js/demo.js" type="text/javascript"></script>-->
     <?php $this->endBody() ?>
-</body>
+    </body>
 </html>
 <?php $this->endPage() ?>
